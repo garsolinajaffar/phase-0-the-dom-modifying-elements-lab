1b12a451ae85e5144f07f34478795445bb56027d
JAFFAR NGAIRA <garsolinajaffar@gmail.com>
2023-04-11 15:52:24 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+// remove the <main> node with id 'main'
+document.querySelector("#main").remove();
+
+// create a new <h1> node with id 'victory' and the text "YOUR-NAME is the champion"
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "YOUR-NAME is the champion";
+
+// add the new <h1> node to the body of the HTML
+document.body.appendChild(newHeader);
